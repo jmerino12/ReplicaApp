d58f6b312f5127f849e6e13d7d52878b0a9c1874
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2020-04-24 12:07:58 -0500

    Dashboard

--- app/src/main/java/com/jonathan/reto2jonathan/Dashboard.java
+++ app/src/main/java/com/jonathan/reto2jonathan/Dashboard.java
@@ -1,8 +1,17 @@
 package com.jonathan.reto2jonathan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.jonathan.reto2jonathan.ui.Favourites;
+import com.jonathan.reto2jonathan.ui.ForYou;
+import com.jonathan.reto2jonathan.ui.HeadLines;
+import com.jonathan.reto2jonathan.ui.NewsStand;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,5 +19,37 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ForYou()).commit();
+        }
+
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_me:
+                    selectedFragment = new ForYou();
+                    break;
+                case R.id.nav_headlines:
+                    selectedFragment = new HeadLines();
+                    break;
+                case R.id.nav_fav:
+                    selectedFragment = new Favourites();
+                    break;
+                case R.id.nav_newsstand:
+                    selectedFragment = new NewsStand();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+            return true;
+        }
+    };
 }
--- app/src/main/java/com/jonathan/reto2jonathan/MainActivity.java
+++ app/src/main/java/com/jonathan/reto2jonathan/MainActivity.java
@@ -2,7 +2,9 @@ package com.jonathan.reto2jonathan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         google = findViewById(R.id.textView2);
         google.setText(getText(R.string.google));
+    }
+
+    public void Dashboard(View view) {
+        startActivity(new Intent(MainActivity.this,Dashboard.class));
     }
 }
